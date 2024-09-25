bd02d893da6a893394388095a8d18bfece9e5359
phani_a <pindra829@gmail.com>
2024-09-25 12:21:10 -0500

    requirements and setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Phani",
+    author_email="pindra829@gmail.com",
+    packages=find_packages()
+)
